9a1bcdb5f1e846a976153d69b2eb76cf05d5aad4
Fadlimnata <fadlitinata@gmail.com>
2025-02-13 11:16:39 +0700

    first commit

---
+++ pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+    $nama ="Fadli Muhammad Rahmatinata";
+    $email = "Fadlitinata@gmail.com";
+    $pekerjaan = "mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "Universitas Pasundan";
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+    echo "<p>Halo, Nama saya $nama, saya adalah seorang $pekerjaan di $jurusan $universitas dan semester 1</p>";
+?>
